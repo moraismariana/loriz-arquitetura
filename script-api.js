709414e36feab0c6cdcf4406b99c22197f778f7f
moraismariana <moraismariana200@gmail.com>
2024-10-28 16:08:38 -0300

    Apresentação conteúdo da API / Pagina Inicial / Sobre

---
+++ script-api.js
@@ -0,0 +1,39 @@
+// Dados da API
+// mudar urls e linha de class visible
+
+Promise.all([
+  fetch("http://127.0.0.1:8000/paginainicialtexto/1/").then((res) =>
+    res.json()
+  ),
+  fetch("http://127.0.0.1:8000/paginainicialimagem/1/").then((res) =>
+    res.json()
+  ),
+  fetch("http://127.0.0.1:8000/paginainicialbg/1/").then((res) => res.json()),
+]).then(([textos, imagens, backgrounds]) => {
+  // mudar essa linha
+  document.getElementById("pagina-inicial-content").classList.add("visible");
+
+  // TEXTOS
+  let textAPI = Object.values(textos);
+  textAPI.shift();
+  let textHTML = document.getElementsByClassName("text");
+  for (let i = 0; i < textHTML.length; i++) {
+    textHTML[i].innerText = textAPI[i];
+  }
+
+  // IMAGENS DO HTML
+  let imgAPI = Object.values(imagens);
+  imgAPI.shift();
+  let imgHTML = document.getElementsByClassName("img");
+  for (let i = 0; i < imgHTML.length; i++) {
+    imgHTML[i].src = imgAPI[i];
+  }
+
+  // BACKGROUNDS
+  let bgAPI = Object.values(backgrounds);
+  bgAPI.shift();
+  let bgHTML = document.getElementsByClassName("bg");
+  for (let i = 0; i < bgHTML.length; i++) {
+    bgHTML[i].style.backgroundImage = `url(${bgAPI[i]})`;
+  }
+});
